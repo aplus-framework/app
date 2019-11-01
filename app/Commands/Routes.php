a9d2763286f3dadc57da9c350a8bc1bc1c8f194e
Natan Felles <natanfelles@gmail.com>
2019-11-01 20:17:16 -0300

    Add "order by" option to routes command

--- app/Commands/Routes.php
+++ app/Commands/Routes.php
@@ -8,17 +8,20 @@ class Routes extends Command
 {
 	protected $name = 'routes';
 	protected $description = 'Show routes list.';
-	protected $usage = 'routes';
+	protected $usage = 'routes [options]';
+	protected $options = [
+		'--order' => 'Order by column',
+	];
 
 	public function run(array $options = [], array $arguments = []) : void
 	{
-		$all = [];
+		$body = [];
 		foreach (\App::getRouter()->getRoutes() as $method => $routes) {
 			foreach ($routes as $route) {
 				/**
 				 * @var Route $route
 				 */
-				$all[] = [
+				$body[] = [
 					$method,
 					$route->getOrigin(),
 					$route->getPath(),
@@ -27,6 +30,12 @@ class Routes extends Command
 				];
 			}
 		}
-		CLI::table($all, ['Method', 'Origin', 'Path', 'Action', 'Name']);
+		$index = $options['order'] ?? 1;
+		\usort($body, static function ($str1, $str2) use ($index) {
+			return \strcmp($str1[$index], $str2[$index]);
+		});
+		$titles = ['Method', 'Origin', 'Path', 'Action', 'Name'];
+		$titles[$index] = CLI::style($titles[$index], CLI::FG_WHITE, null, ['bold']);
+		CLI::table($body, $titles);
 	}
 }
