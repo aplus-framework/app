ec6640a28bc0b7cdda40c1ae8dad6d740f913d83
Natan Felles <natanfelles@gmail.com>
2021-04-07 10:31:28 -0300

    Use Response::setStatusLine to also set the reason

--- app/Controllers/Errors.php
+++ app/Controllers/Errors.php
@@ -4,7 +4,7 @@ class Errors extends BaseController
 {
 	public function notFound()
 	{
-		$this->response->setStatusCode(404);
+		$this->response->setStatusLine(404);
 		return $this->renderPage('errors/not-found', [
 			'title' => lang('errors.404-notFound'),
 			'home_url' => route_url('home', [], [8080]), // TODO: origin
