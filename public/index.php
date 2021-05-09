285121f96020c675d7003755fc01944be0957066
Natan Felles <natanfelles@gmail.com>
2021-05-09 12:05:13 -0300

    Pass constants file load to composer

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
 use Framework\MVC\Config;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../config/constants.php';
-require __DIR__ . '/../App.php';
 App::init(new Config(CONFIG_DIR));
 App::run();
