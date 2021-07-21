64f0eb1d467d6c82cde8995be5486b7ca14f4e39
Natan Felles <natanfelles@gmail.com>
2021-07-21 18:24:07 -0300

    Use Route Attribute on Home Controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,13 @@
 <?php namespace App\Controllers;
 
 use Framework\MVC\Controller;
+use Framework\Routing\Attributes\Route;
 
 final class Home extends Controller
 {
-	public function index() : string
-	{
-		return view('home/index');
-	}
+    #[Route('GET', '/', name: 'home')]
+    public function index() : string
+    {
+        return view('home/index');
+    }
 }
