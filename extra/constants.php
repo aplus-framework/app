75887d29c5e452d86e0468e0e0d68ad3b15b4f65
Natan Felles <natanfelles@gmail.com>
2021-07-21 18:34:40 -0300

    Add the EXTRA_DIR constant

--- extra/constants.php
+++ extra/constants.php
@@ -3,5 +3,6 @@ define('ENVIRONMENT', $_SERVER['ENVIRONMENT'] ?? 'production');
 define('ROOT_DIR', dirname(__DIR__) . '/');
 define('APP_DIR', ROOT_DIR . 'app/');
 define('CONFIG_DIR', ROOT_DIR . 'config/');
+define('EXTRA_DIR', ROOT_DIR . 'extra/');
 define('PUBLIC_DIR', ROOT_DIR . 'public/');
 define('STORAGE_DIR', ROOT_DIR . 'storage/');
