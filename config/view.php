d477dcca6309e2a33467888baac3b8ad19f82893
Natan Felles <natanfelles@gmail.com>
2021-07-22 11:06:36 -0300

    Update view config

--- config/view.php
+++ config/view.php
@@ -2,11 +2,11 @@
 /**
  * View config.
  *
- * @see App::view
+ * @see App::view()
  */
 return [
-	'default' => [
-		'base_path' => APP_DIR . 'Views',
-		'extension' => '.php',
-	],
+    'default' => [
+        'base_dir' => APP_DIR . 'Views',
+        'extension' => '.php',
+    ],
 ];
