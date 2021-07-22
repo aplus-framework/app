be3084230abe6212fbdd06109e7432028e6431c1
Natan Felles <natanfelles@gmail.com>
2021-07-22 11:25:15 -0300

    Update home/index view

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,11 +1,20 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" dir="ltr">
 <head>
-	<meta charset="utf-8">
-	<title>Framework App</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aplus Framework</title>
+    <style>
+        body {
+            background: #8892bf;
+            color: #000;
+            font-family: monospace;
+            margin: 1em;
+        }
+    </style>
 </head>
 <body>
-<h1>Framework App</h1>
+<h1>Aplus Framework</h1>
 <p>The app is running! Yep!</p>
 </body>
 </html>
