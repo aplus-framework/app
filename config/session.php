2a4f5b8f65f057911bc50ae7c26fd3f5b6029c66
Natan Felles <natanfelles@gmail.com>
2021-07-22 11:45:53 -0300

    Use FilesHandler as default session save handler

--- config/session.php
+++ config/session.php
@@ -3,17 +3,26 @@
  * Session config.
  *
  * @see App::session()
+ * @see Framework\Session\Session::setOptions()
  * @see Framework\Session\SaveHandlers\DatabaseHandler::prepareConfig()
  * @see Framework\Session\SaveHandlers\FilesHandler::prepareConfig()
  * @see Framework\Session\SaveHandlers\MemcachedHandler::prepareConfig()
  * @see Framework\Session\SaveHandlers\RedisHandler::prepareConfig()
  */
+
+use Framework\Session\SaveHandlers\FilesHandler;
+
 return [
     'default' => [
         'options' => [],
         'save_handler' => [
-            'class' => null,
-            'config' => [],
+            'class' => FilesHandler::class,
+            'config' => [
+                'prefix' => '',
+                'directory' => STORAGE_DIR . 'sessions',
+                'match_ip' => false,
+                'match_ua' => false,
+            ],
         ],
     ],
 ];
