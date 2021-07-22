30da795a1ce5f1edecfde5598517e2b2fe7268f5
Natan Felles <natanfelles@gmail.com>
2021-07-22 11:30:48 -0300

    Use DIRECTORY_SEPARATOR as last separator on *_DIR constants

--- extra/constants.php
+++ extra/constants.php
@@ -1,8 +1,8 @@
 <?php
 define('ENVIRONMENT', $_SERVER['ENVIRONMENT'] ?? 'production');
-define('ROOT_DIR', dirname(__DIR__) . '/');
-define('APP_DIR', ROOT_DIR . 'app/');
-define('CONFIG_DIR', ROOT_DIR . 'config/');
-define('EXTRA_DIR', ROOT_DIR . 'extra/');
-define('PUBLIC_DIR', ROOT_DIR . 'public/');
-define('STORAGE_DIR', ROOT_DIR . 'storage/');
+define('ROOT_DIR', dirname(__DIR__) . \DIRECTORY_SEPARATOR);
+define('APP_DIR', ROOT_DIR . 'app' . \DIRECTORY_SEPARATOR);
+define('CONFIG_DIR', ROOT_DIR . 'config' . \DIRECTORY_SEPARATOR);
+define('EXTRA_DIR', ROOT_DIR . 'extra' . \DIRECTORY_SEPARATOR);
+define('PUBLIC_DIR', ROOT_DIR . 'public' . \DIRECTORY_SEPARATOR);
+define('STORAGE_DIR', ROOT_DIR . 'storage' . \DIRECTORY_SEPARATOR);
