20586f756208038850d7348293ca123ba4b2b9e8
Natan Felles <natanfelles@gmail.com>
2021-07-31 13:18:53 -0300

    Add cryptography helpers

---
+++ config/crypto.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Crypto config.
+ *
+ * @see encrypt()
+ * @see decrypt()
+ * @see https://aplus-framework.com/docs/app/config#crypto
+ */
+return [
+    'default' => [
+        'key' => 'secret',
+    ],
+];
