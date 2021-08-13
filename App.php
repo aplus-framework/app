801c9d2e7627e1763cedbcb2fd71b7047d8f2f54
Natan Felles <natanfelles@gmail.com>
2021-08-13 19:07:15 -0300

    Add PHPDoc on App

--- App.php
+++ App.php
@@ -1,5 +1,12 @@
 <?php declare(strict_types=1);
 
+/**
+ * Class App.
+ *
+ * @package app
+ *
+ * @see https://docs.aplus-framework.com/guides/projects/app/#the-global-class-app
+ */
 class App extends \Framework\MVC\App
 {
 }
