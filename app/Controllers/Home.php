062d2aef5a342865333b95c51b4c539e81ad9611
Natan Felles <natanfelles@gmail.com>
2021-08-24 05:28:26 -0300

    Add PHPDocs on Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,8 +3,16 @@
 use Framework\MVC\Controller;
 use Framework\Routing\Attributes\Route;
 
+/**
+ * Class Home.
+ *
+ * @package app
+ */
 final class Home extends Controller
 {
+    /**
+     * Renders the application homepage.
+     */
     #[Route('GET', '/', name: 'home')]
     public function index() : string
     {
