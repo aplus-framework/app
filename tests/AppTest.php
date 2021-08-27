735fcb55ef0662f398c3e9bdfadfe7362bd80888
Natan Felles <natanfelles@gmail.com>
2021-08-27 07:32:35 -0300

    Include App class in unit tests

---
+++ tests/AppTest.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of App Project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests;
+
+use App;
+use Framework\Config\Config;
+
+/**
+ * Class AppTest.
+ *
+ * @runTestsInSeparateProcesses
+ */
+final class AppTest extends TestCase
+{
+    protected function setUp() : void
+    {
+        //
+    }
+
+    public function testInstance() : void
+    {
+        self::assertInstanceOf(
+            \Framework\MVC\App::class,
+            new App(new Config(CONFIG_DIR))
+        );
+    }
+}
