856422e32e40d9502cabbe66e3208cf1ded098cb
Natan Felles <natanfelles@gmail.com>
2021-09-05 20:08:08 -0300

    Add details on the homepage

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -11,10 +11,31 @@
             font-family: monospace;
             margin: 1em;
         }
+
+        details {
+            margin-top: 3em;
+        }
     </style>
 </head>
 <body>
 <h1><?= lang('home.title') ?></h1>
 <p><?= lang('home.description') ?></p>
+<details>
+    <summary><?= Aplus::DESCRIPTION ?></summary>
+    <p>Codename: <code><?= Aplus::CODENAME ?></code></p>
+    <p>Version: <code><?= Aplus::VERSION ?></code></p>
+    <h2>Declarations</h2>
+    <ol>
+        <?php foreach (Framework\Autoload\Preloader::getDeclarations() as $declaration): ?>
+            <li><?= $declaration ?></li>
+        <?php endforeach ?>
+    </ol>
+    <h2>Included Files</h2>
+    <ol>
+        <?php foreach (Framework\Autoload\Preloader::getIncludedFiles() as $includedFile): ?>
+            <li><?= $includedFile ?></li>
+        <?php endforeach ?>
+    </ol>
+</details>
 </body>
 </html>
