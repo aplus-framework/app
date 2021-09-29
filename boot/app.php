956867ef20ea0716cb4570f94d44b36413adcaaf
Natan Felles <natanfelles@gmail.com>
2021-09-29 17:54:01 -0300

    We need to boot files with composer for testing
    This reverts commit b8831ed5

--- boot/app.php
+++ boot/app.php
@@ -10,10 +10,11 @@ date_default_timezone_set('UTC');
 
 if (is_file(__DIR__ . '/../vendor/autoload.php')) {
     require __DIR__ . '/../vendor/autoload.php';
+} else {
+    require __DIR__ . '/../extra/constants.php';
+    require EXTRA_DIR . 'helpers.php';
+    require ROOT_DIR . 'App.php';
 }
-require __DIR__ . '/../extra/constants.php';
-require EXTRA_DIR . 'helpers.php';
-require ROOT_DIR . 'App.php';
 
 use Framework\Config\Config;
 
