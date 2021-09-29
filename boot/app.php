06511f3419d8431d579ef4d266cdcac13e8753dc
Natan Felles <natanfelles@gmail.com>
2021-09-29 15:35:26 -0300

    Move boot file

--- boot/app.php
+++ boot/app.php
@@ -5,11 +5,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-if (is_file(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
+if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
 } else {
-    require __DIR__ . '/extra/init.php';
-    require __DIR__ . '/extra/constants.php';
+    require __DIR__ . '/../extra/init.php';
+    require __DIR__ . '/../extra/constants.php';
     require EXTRA_DIR . 'helpers.php';
     require ROOT_DIR . 'App.php';
 }
--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-(require __DIR__ . '/../boot.php')->runHttp();
+(require __DIR__ . '/../boot/app.php')->runHttp();
