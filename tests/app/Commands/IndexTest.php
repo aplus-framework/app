18eccdbc2ac309b032baaf314841c7e96b3b479a
Natan Felles <natanfelles@gmail.com>
2021-09-29 19:55:35 -0300

    Test index command

---
+++ tests/app/Commands/IndexTest.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of App Project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\app\Commands;
+
+use Tests\TestCase;
+
+/**
+ * Class IndexTest.
+ *
+ * @runTestsInSeparateProcesses
+ */
+final class IndexTest extends TestCase
+{
+    public function testRun() : void
+    {
+        $this->app->runCli('index');
+        self::assertStdoutContains('index');
+    }
+}
