058e2ae676e67cc8561ede78539c52aa8c88f231
Natan Felles <natanfelles@gmail.com>
2021-09-29 17:23:29 -0300

    Update views

--- app/Views/errors/404.php
+++ app/Views/errors/404.php
@@ -4,12 +4,13 @@
  * @var string $title
  * @var Framework\MVC\View $view
  */
+$view->extends('default');
+$view->block('contents');
 ?>
-<?php $view->extends('default') ?>
-<?php $view->block('contents') ?>
-<h1><?= $title ?></h1>
-<p><?= $message ?></p>
-<p>
-    <a href="<?= route_url('home') ?>">Go to homepage</a>
-</p>
-<?php $view->endBlock() ?>
+    <h1><?= $title ?></h1>
+    <p><?= $message ?></p>
+    <p>
+        <a href="<?= route_url('home') ?>">Go to homepage</a>
+    </p>
+<?php
+$view->endBlock();
--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -2,27 +2,27 @@
 /**
  * @var Framework\MVC\View $view
  */
+$view->extends('default');
+$view->block('contents');
 ?>
-<?php $view->extends('default') ?>
-<?php $view->block('contents') ?>
-<h1><?= lang('home.title') ?></h1>
-<p><?= lang('home.description') ?></p>
-<details>
-    <summary><?= Aplus::DESCRIPTION ?></summary>
-    <p>Codename: <code><?= Aplus::CODENAME ?></code></p>
-    <p>Version: <code><?= Aplus::VERSION ?></code></p>
-    <h2>Declarations</h2>
-    <ol>
-        <?php foreach (Framework\Autoload\Preloader::getDeclarations() as $declaration): ?>
-            <li><?= $declaration ?></li>
-        <?php endforeach ?>
-    </ol>
-    <h2>Included Files</h2>
-    <ol>
-        <?php foreach (Framework\Autoload\Preloader::getIncludedFiles() as $includedFile): ?>
-            <li><?= $includedFile ?></li>
-        <?php endforeach ?>
-    </ol>
-</details>
-<?php $view->endBlock() ?>
-
+    <h1><?= lang('home.title') ?></h1>
+    <p><?= lang('home.description') ?></p>
+    <details>
+        <summary><?= Aplus::DESCRIPTION ?></summary>
+        <p>Codename: <code><?= Aplus::CODENAME ?></code></p>
+        <p>Version: <code><?= Aplus::VERSION ?></code></p>
+        <h2>Declarations</h2>
+        <ol>
+            <?php foreach (Framework\Autoload\Preloader::getDeclarations() as $declaration): ?>
+                <li><?= $declaration ?></li>
+            <?php endforeach ?>
+        </ol>
+        <h2>Included Files</h2>
+        <ol>
+            <?php foreach (Framework\Autoload\Preloader::getIncludedFiles() as $includedFile): ?>
+                <li><?= $includedFile ?></li>
+            <?php endforeach ?>
+        </ol>
+    </details>
+<?php
+$view->endBlock();
