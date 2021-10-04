8620800343c165c9c2924355929f9cefdd9c0a7c
Natan Felles <natanfelles@gmail.com>
2021-10-04 02:22:14 -0300

    Add bin/ps-meta to generate PhpStorm Meta file

---
+++ .phpstorm.meta.php
@@ -0,0 +1,98 @@
+<?php namespace PHPSTORM_META;
+
+// Configs
+registerArgumentsSet(
+    'configs',
+    'anti-csrf',
+    'autoloader',
+    'cache',
+    'console',
+    'crypto',
+    'database',
+    'exceptions',
+    'language',
+    'locator',
+    'logger',
+    'mailer',
+    'request',
+    'response',
+    'router',
+    'routes',
+    'session',
+    'validation',
+    'view',
+);
+expectedArguments(
+    \config(),
+    0,
+    argumentsSet('configs')
+);
+
+// Helpers
+registerArgumentsSet(
+    'helpers',
+);
+expectedArguments(
+    \helpers(),
+    0,
+    argumentsSet('helpers')
+);
+
+// Languages
+registerArgumentsSet(
+    'langs',
+    'home.description',
+    'home.title',
+);
+expectedArguments(
+    \lang(),
+    0,
+    argumentsSet('langs')
+);
+
+// Routes
+registerArgumentsSet(
+    'routes',
+    'home',
+);
+expectedArguments(
+    \route_url(),
+    0,
+    argumentsSet('routes')
+);
+
+// Views
+registerArgumentsSet(
+    'views',
+    'errors/404',
+    'home/index',
+);
+expectedArguments(
+    \view(),
+    0,
+    argumentsSet('views')
+);
+
+registerArgumentsSet(
+    'views_layouts',
+    'default',
+);
+expectedArguments(
+    \Framework\MVC\View::extends(),
+    0,
+    argumentsSet('views_layouts')
+);
+expectedArguments(
+    \Framework\MVC\View::isExtending(),
+    0,
+    argumentsSet('views_layouts')
+);
+
+registerArgumentsSet(
+    'views_includes',
+);
+expectedArguments(
+    \Framework\MVC\View::include(),
+    0,
+    argumentsSet('views_includes')
+);
