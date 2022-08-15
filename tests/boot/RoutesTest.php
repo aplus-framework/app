92dbc0f49449eb95d4fd1ce5799c5f109a0ab5ff
Natan Felles <natanfelles@gmail.com>
2022-08-15 17:05:15 -0300

    Move routes test to boot folder

--- tests/boot/RoutesTest.php
+++ tests/boot/RoutesTest.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Tests\extra;
+namespace Tests\boot;
 
 use Tests\TestCase;
 
