38074f99f14d9775665e3ef252b472a48e9467b1
Natan Felles <natanfelles@gmail.com>
2023-10-13 17:01:44 -0300

    Does not preload Composer autoloader
    Composer fails to load some classes if it is preloaded

--- preload.php
+++ preload.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/aplus/autoload/src/Preloader.php';
 
 use Framework\Autoload\Preloader;
 
